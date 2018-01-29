a46abbfd8eacba9a0711f059925bc52beaff0a54
Yang Yongli <yongli.yang@dianping.com>
2018-01-29 14:41:53 +0800

    initial with mysql

---
+++ src/main/java/com/code/essay/CodeEssayApplication.java
@@ -0,0 +1,12 @@
+package com.code.essay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeEssayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeEssayApplication.class, args);
+	}
+}
